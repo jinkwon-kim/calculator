64fc731fb9bf019cf8163295f070a9d48a62fb9d
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-04-20 22:01:03 +0900

    3일차 드디어 완성

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -6,18 +6,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int result = 0;
+        while (true){
         //1번째 숫자 변수에 넣기
         System.out.println("첫번째 숫자를 입력하세요.");
-        int num1 = scan.nextInt();
+        int num1 = Integer.parseInt(scan.nextLine());
         //2번째 숫자 변수에 넣기
         System.out.println("두번째 숫자를 입력하세요.");
-        int num2 = scan.nextInt();
+        int num2 = Integer.parseInt(scan.nextLine());
         //사칙연산 기호 변수에 넣기
         System.out.println("사칙연산 기호를 입력하세요");
-        char symbol = scan.nextShort().charAt();
+        char symbol = scan.nextLine().charAt(0);
         //계산기 기능 넣기
         switch (symbol){
-            int result = 0;
             case '+':
                 result = num1 + num2;
                 break;
@@ -29,8 +30,18 @@ public class App {
                 break;
             case '/':
                 result = num1 / num2;
+                break;}
                 System.out.println("결과 = " + result);
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String input = scan.nextLine();
+                if (input.equals("exit")){
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
+
+            }
+
 
         }
     }
-}
+
